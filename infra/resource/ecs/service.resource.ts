e9660f62673838271666d6bc6d0a358f1a6d6c57
JohannMeza <johanmezasalazar@gmail.com>
2026-07-29 01:31:05 -0500

    chore: add default networkConfiguration to ECS service for Fargate

--- infra/resource/ecs/service.resource.ts
+++ infra/resource/ecs/service.resource.ts
@@ -10,6 +10,43 @@ export interface ServiceArgs {
 }
 
 export function createService(args: ServiceArgs): aws.ecs.Service {
+  // Obtener la VPC por defecto de Localstack
+  const defaultVpc = aws.ec2.getVpc({ default: true }, { provider });
+
+  // Obtener las subredes por defecto en la VPC
+  const defaultSubnets = defaultVpc.then((vpc) =>
+    aws.ec2.getSubnets(
+      {
+        filters: [
+          {
+            name: 'vpc-id',
+            values: [vpc.id],
+          },
+        ],
+      },
+      { provider },
+    ),
+  );
+
+  // Obtener el grupo de seguridad por defecto
+  const defaultSecurityGroup = defaultVpc.then((vpc) =>
+    aws.ec2.getSecurityGroup(
+      {
+        filters: [
+          {
+            name: 'vpc-id',
+            values: [vpc.id],
+          },
+          {
+            name: 'group-name',
+            values: ['default'],
+          },
+        ],
+      },
+      { provider },
+    ),
+  );
+
   return new aws.ecs.Service(
     `${args.serviceName}-service`,
     {
@@ -21,6 +58,11 @@ export function createService(args: ServiceArgs): aws.ecs.Service {
       deploymentMinimumHealthyPercent: 50,
       deploymentMaximumPercent: 200,
       enableExecuteCommand: true,
+      networkConfiguration: {
+        assignPublicIp: true,
+        subnets: defaultSubnets.then((s) => s.ids),
+        securityGroups: [defaultSecurityGroup.then((sg) => sg.id)],
+      },
     },
     {
       provider,
